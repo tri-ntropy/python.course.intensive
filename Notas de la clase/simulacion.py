c3f4d9ffd97e8562390a99d8a0b180261fe1fad3
tri-ntropy <tony.riveraisl@gmail.com>
2025-06-11 14:18:04 -0600

    Bloque 3 Agregado

---
+++ Notas de la clase/simulacion.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def euler(t0, tf, x0, v0, n, fuerza = lambda x, v, i: 0):
+    tiempo, paso_tiempo = np.linspace(t0, tf, n + 1, retstep = True)
+
+    velocidad = np.zeros(n + 1)
+    desplazamiento = np.zeros(n + 1)
+
+    velocidad[0] = v0
+    desplazamiento[0] = x0
+
+    for i in range(1, n + 1):
+        velocidad[i] = velocidad[i - 1] + paso_tiempo * fuerza(desplazamiento, velocidad, i)
+        desplazamiento[i] = desplazamiento[i - 1] + paso_tiempo * velocidad[i]
+
+    return {"x" : desplazamiento,
+            "v" : velocidad,
+            "t" : tiempo,
+            "dt" : paso_tiempo}
+
+
+def graficar_desplazamiento(t, x):
+    plt.figure(figsize = (6, 2.5))
+    plt.plot(t, x)
+    plt.ylabel("$x(t)$")
+    plt.xlabel("$t$")
+    plt.show()
+
+def graficar_velocidad(t, v):
+    plt.figure(figsize = (6, 2.5))
+    plt.plot(t, v)
+    plt.ylabel("$v(t)$")
+    plt.xlabel("$t$")
+    plt.show()
